947ac1f0646ccfc1fba8f31e111c351389d26375
anhnbt-it <anhnbt.it@gmail.com>
2020-10-19 08:30:39 +0700

    first commit

---
+++ src/GenericStackClient.java
@@ -0,0 +1,15 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: AnhNBT (anhnbt.it@gmail.com)
+ * Date: 10/19/2020
+ * Time: 8:26 AM
+ */
+public class GenericStackClient {
+    public void stackOfStrings() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void stackOfIntegers() {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/MyGenericStack.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: AnhNBT (anhnbt.it@gmail.com)
+ * Date: 10/19/2020
+ * Time: 8:24 AM
+ */
+public class MyGenericStack<T> {
+
+    LinkedList<T> stack;
+
+    public MyGenericStack() {
+    }
+
+    public void push(T element) {
+        throw new UnsupportedOperationException();
+    }
+
+    public T pop() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int size() {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException();
+    }
+}
