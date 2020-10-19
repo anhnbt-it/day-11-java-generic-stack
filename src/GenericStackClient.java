4170311b2ee555dce6850ffc24c242c8172f1afc
anhnbt-it <anhnbt.it@gmail.com>
2020-10-19 08:53:20 +0700

    test: add main test Stack String and Integer

--- src/GenericStackClient.java
+++ src/GenericStackClient.java
@@ -5,11 +5,41 @@
  * Time: 8:26 AM
  */
 public class GenericStackClient {
-    public void stackOfStrings() {
-        throw new UnsupportedOperationException();
+    public static void stackOfStrings() {
+        MyGenericStack<String> stack = new MyGenericStack<>();
+        stack.push("Five");
+        stack.push("Four");
+        stack.push("Three");
+        stack.push("Two");
+        stack.push("One");
+        System.out.println("\tSize of stack after"
+        + " push operations: " + stack.size());
+        System.out.print("\tPop elements from stack:");
+        while (!stack.isEmpty()) {
+            System.out.printf(" %s", stack.pop());
+        }
+        System.out.println("\n\tSize of stack pop operations: " + stack.size());
     }
 
-    public void stackOfIntegers() {
-        throw new UnsupportedOperationException();
+    public static void stackOfIntegers() {
+        MyGenericStack<Integer> stack = new MyGenericStack<>();
+        stack.push(5);
+        stack.push(4);
+        stack.push(3);
+        stack.push(2);
+        stack.push(1);
+        System.out.println("\tSize of stack after push operations: " + stack.size());
+        System.out.print("\tPop elements from stack:");
+        while(!stack.isEmpty()) {
+            System.out.printf(" %d", stack.pop());
+        }
+        System.out.println("\n\tSize of stack after pop operations: " + stack.size());
+    }
+
+    public static void main(String[] args) {
+        System.out.println("1. Stack of integers");
+        stackOfIntegers();
+        System.out.println("\n2. Stack of Strings");
+        stackOfStrings();
     }
 }
