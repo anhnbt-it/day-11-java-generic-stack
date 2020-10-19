59c74e4629b3217066378d2c671454c4c843d663
anhnbt-it <anhnbt.it@gmail.com>
2020-10-19 08:52:31 +0700

    refactor: add source code to MyGenericStack

--- src/MyGenericStack.java
+++ src/MyGenericStack.java
@@ -1,3 +1,4 @@
+import java.util.EmptyStackException;
 import java.util.LinkedList;
 
 /**
@@ -8,24 +9,31 @@ import java.util.LinkedList;
  */
 public class MyGenericStack<T> {
 
-    LinkedList<T> stack;
+    private LinkedList<T> stack;
 
     public MyGenericStack() {
+        stack = new LinkedList<>();
     }
 
     public void push(T element) {
-        throw new UnsupportedOperationException();
+        stack.addFirst(element);
     }
 
     public T pop() {
-        throw new UnsupportedOperationException();
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return stack.removeFirst();
     }
 
     public int size() {
-        throw new UnsupportedOperationException();
+        return stack.size();
     }
 
     public boolean isEmpty() {
-        throw new UnsupportedOperationException();
+        if (stack.size() == 0) {
+            return true;
+        }
+        return false;
     }
 }
